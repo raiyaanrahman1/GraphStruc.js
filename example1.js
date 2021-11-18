4da1f154b1dbc5666335d3810664d4aae4aa9465
Raiyaan Rahman <raiyaan.rahman01@gmail.com>
2021-11-18 01:24:25 -0500

    vertices pretty much done

--- example1.js
+++ example1.js
@@ -3,7 +3,23 @@ let graphDiv = document.getElementById("graph");
 // the graphDiv Element must be on the document before calling new GraphStruc
 // If you create the element in JS, make sure to append the element to the document body before calling new GraphStruc()
 // calling new GraphStruc() will automatically append it to the element passed in
-let graph = new GraphStruc(graphDiv);
-for(let i = 0; i < 7; i++){
+// This helps the library convert units, and makes it more responsive
+// The graphDiv element should be a container for only the GraphStruc, nothing else, since the GraphStruct will take up 100%
+// of the element's space 
+let graph = new GraphStruc(graphDiv, 3);
+for(let i = 0; i < 9; i++){
     graph.createVertex(i, "50px");
 }
+
+
+// DON'T DO:
+// graph.createVertex(10000, "50px", ["50px", "50px"]);
+// graph.createVertex(10001, "50px", ["1200px", "300px"]);
+
+// DO:
+graph.createVertex(10000, "50px", ["5%", "15%"]);
+graph.createVertex(10001, "5%", ["95%", "85%"]);
+
+// Shouldn't specify position in pixels for absolute positioning, since the position is calculated from the top and
+// left of the graph bounding box, if the user resizes the window from the bottom or right, the position won't be 
+// adjusted. So always specify the position
--- graphStruc.js
+++ graphStruc.js
@@ -1,39 +1,62 @@
 const LENGTH_REGEX = /^[0-9]+((px)|(%)|(cm)|(mm)|(in)|(pt)|(pc)|(em)|(rem)|(ex)|(vw)|(vh))(?!.)/;
 const UNIT_REGEX = /((px)|(%)|(cm)|(mm)|(in)|(pt)|(pc)|(em)|(rem)|(vw)|(vh))/;
 class GraphStruc {
-    constructor(parentElem){
+    constructor(parentElem, maxNodesPerRow = Infinity){
         this.parentElem = parentElem;
         this.graphElem = document.createElement("div");
         this.vertices = [];
+        this.numDefaultVert = 0;
         this.edges = [];
         this.graphElem.style = `width: 100%; height: 100%; border: 1px solid;`
-        this.width = window.getComputedStyle(parentElem).width.split(UNIT_REGEX)[0]; // in pixels
-        this.height = window.getComputedStyle(parentElem).height.split(UNIT_REGEX)[0]; // in pixels
-        this.graphElem.style.position = "relative"
+        this.width = parseFloat(window.getComputedStyle(parentElem).width.split(UNIT_REGEX)[0]); // in pixels
+        this.height = parseFloat(window.getComputedStyle(parentElem).height.split(UNIT_REGEX)[0]); // in pixels
+        this.graphElem.style.position = "relative";
+        this.maxNodesPerRow = maxNodesPerRow;
 
         this.parentElem.append(this.graphElem);
         window.addEventListener("resize", () => {
-            this.width = window.getComputedStyle(parentElem).width.split(UNIT_REGEX)[0];
-            this.height = window.getComputedStyle(parentElem).height.split(UNIT_REGEX)[0];
-            let diamMag, xPos, yPos;
+            this.width = parseFloat(window.getComputedStyle(parentElem).width.split(UNIT_REGEX)[0]);
+            this.height = parseFloat(window.getComputedStyle(parentElem).height.split(UNIT_REGEX)[0]);
+            let numRows = this.maxNodesPerRow === Infinity ? 1 : Math.ceil(this.numDefaultVert / this.maxNodesPerRow);
+
+            let j = 0;
             for(let i = 0; i < this.vertices.length; i++){
-                diamMag = this.vertices[i].diameter.split(UNIT_REGEX)[0];
-                xPos = (i+1) / (this.vertices.length + 1) * this.width - diamMag/2;
-                yPos = this.height / 2 - diamMag / 2;
-                this.vertices[i].updatePosition([xPos + "px", yPos + "px"]);
+                let diamMag = parseFloat(this.vertices[i].diameter.split(UNIT_REGEX)[0]);
+
+                if(this.vertices[i].isDefault){
+                    let rowNum = Math.floor(j / this.maxNodesPerRow);
+
+                    let value = this.maxNodesPerRow === Infinity ? this.numDefaultVert : this.maxNodesPerRow;
+        
+                    let xPos = (j % this.maxNodesPerRow + 1) / (value + 1) * this.width - diamMag/2;
+                    let yPos = (rowNum + 1) / (numRows + 1) * this.height - diamMag / 2;
+                    this.vertices[i].updatePosition([xPos + "px", yPos + "px"]);
+                    j++;
+                }
+                else {
+                    let [xPos, yPos] = this.vertices[i].positionOriginal;
+                    let [xMag, xUnit] = xPos.split(UNIT_REGEX);
+                    let [yMag, yUnit] = yPos.split(UNIT_REGEX);
+                    xMag = parseFloat(xMag);
+                    yMag = parseFloat(yMag);
+                    xMag = this.convertToPixel(xMag, xUnit);
+                    yMag = this.convertToPixel(yMag, yUnit, this.height);
+                    let position = [(xMag - diamMag / 2) + "px", (yMag - diamMag / 2) + "px"];
+                    this.vertices[i].updatePosition(position);
+                }
             }
-            // console.log("resized");
-            // console.log(this.width);
-            // console.log(this.height);
             
         })
     
     }
 
-    convertToPixel(xMag, xUnit) {
+    // helper method: not to be called by client
+    convertToPixel(xMag, xUnit, direction = this.width) {
         
         switch(xUnit){
             // absolute units
+            case "px":
+                return xMag;
             case "in":
                 return (xMag*96);
             case "cm":
@@ -46,7 +69,7 @@ class GraphStruc {
                 return (xMag*16);
             // relative units
             case "%":
-                return (xMag / 100) * this.width;
+                return (xMag / 100) * direction;
             case "rem":
                 return (xMag*parseFloat(getComputedStyle(document.documentElement).fontSize));
             case "em":
@@ -73,28 +96,47 @@ class GraphStruc {
             throw new TypeError("Invalid diameter. See supported units.");
         }
 
+        let positionOriginal = position === undefined ? undefined : JSON.parse(JSON.stringify(position));
         let [diamMag, diamUnit] = diameter.split(UNIT_REGEX);
+        diamMag = parseFloat(diamMag);
         if(diamUnit !== "px"){
             diamMag = this.convertToPixel(diamMag, diamUnit);
             
         }
+        let isDefault = position === undefined;
 
         if(position === undefined){
-            // figure out where the vertex should go
-            
-            let xPos = (this.vertices.length + 1) / (this.vertices.length + 2) * this.width - diamMag/2; // from the left
-            let yPos = this.height / 2 - diamMag / 2; // from the top
-            position = [xPos + "px", yPos + "px"];
-            //console.log(position);
+            this.numDefaultVert++;
+            position = ["0px", "0px"];
+        }
+        else {
+            let [xPos, yPos] = position;
+            let [xMag, xUnit] = xPos.split(UNIT_REGEX);
+            let [yMag, yUnit] = yPos.split(UNIT_REGEX);
+            xMag = parseFloat(xMag);
+            yMag = parseFloat(yMag);
+            xMag = this.convertToPixel(xMag, xUnit);
+            yMag = this.convertToPixel(yMag, yUnit, this.height);
+            position = [(xMag - diamMag / 2) + "px", (yMag - diamMag / 2) + "px"]
         }
         
-        let vert = new Vertex(key, this.graphElem, diamMag + "px", position);
+        let vert = new Vertex(key, this.graphElem, diamMag + "px", position, positionOriginal, isDefault);
         this.vertices.push(vert);
+        
+        let numRows = this.maxNodesPerRow === Infinity ? 1 : Math.ceil(this.numDefaultVert / this.maxNodesPerRow);
 
-        for(let i = 0; i < this.vertices.length - 1; i++){
-            let xPos = (i+1) / (this.vertices.length + 1) * this.width - diamMag/2;
-            let yPos = this.height / 2 - diamMag / 2;
-            this.vertices[i].updatePosition([xPos + "px", yPos + "px"]);
+        let j = 0;
+        for(let i = 0; i < this.vertices.length; i++){
+            if(this.vertices[i].isDefault){
+                let rowNum = Math.floor(j / this.maxNodesPerRow);
+
+                let value = this.maxNodesPerRow === Infinity ? this.numDefaultVert : this.maxNodesPerRow;
+    
+                let xPos = (j % this.maxNodesPerRow + 1) / (value + 1) * this.width - diamMag/2;
+                let yPos = (rowNum + 1) / (numRows + 1) * this.height - diamMag / 2;
+                this.vertices[i].updatePosition([xPos + "px", yPos + "px"]);
+                j++;
+            }
         }
         
     }
@@ -106,13 +148,14 @@ class GraphStruc {
 
 class Vertex {
     
-    constructor(key, graphElem, diameter, position){
+    constructor(key, graphElem, diameter, position, positionOriginal, isDefault){
         this.vertexElem = document.createElement("div");
         this.key = key;
         this.position = position;
         this.diameter = diameter;
+        this.isDefault = isDefault;
+        this.positionOriginal = positionOriginal;
         this.vertexElem.setAttribute("style", `width: ${diameter}; height: ${diameter}; border-radius: 50%; position: absolute; left: ${this.position[0]}; top: ${this.position[1]}; border: 1px solid`)
-        //this.vertexElem.style = `width: ${diameter}; height: ${diameter}; border-radius: 50%; position: absolute; left: ${this.position[0]}; top: ${this.position[1]}; border: 1px solid`;
         graphElem.append(this.vertexElem);
     }
 
